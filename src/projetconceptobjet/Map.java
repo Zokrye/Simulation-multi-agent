d968925c7e5ceb71ca9f70c6e1647cdfb906e2d6
Alexandre Canton <a.canton@cleanslateuv.com>
2019-10-17 16:35:04 +0200

    Replaced cells list by a table

--- src/projetconceptobjet/Map.java
+++ src/projetconceptobjet/Map.java
@@ -12,30 +12,45 @@ import java.util.List;
  * @author Alexandre
  */
 public class Map {
+    protected final int SIZE_SAFE_ZONE=5;
     protected final int HEIGHT;
     protected final int LENGTH;
     protected final int NB_OBSTACLES;
-    protected List<Cell> cells;
+    //protected List<Cell> cells;
+    protected Cell[][] cells;
     private static Map map;
     
     public Map(int height, int length, int nbObstacles) {
         this.HEIGHT=height;
         this.LENGTH=length;
+        cells=new Cell[LENGTH][HEIGHT];
         this.NB_OBSTACLES=nbObstacles;  
-        for(int i=0; i<HEIGHT;i++) {
+        /*for(int i=0; i<HEIGHT;i++) {
             for(int j=0;j<LENGTH;j++) {
                 cells.add(new Cell(this,i,j));
             }
+        }*/
+        for(int i=0; i<HEIGHT;i++) {
+            for(int j=0;j<LENGTH;j++) {
+                cells[i][j]=(new Cell(this,i,j));
+            }
         }
+
     }
     
     public Cell getCell(int x, int y) {
-        for(Cell cell : cells) {
+        /*for(Cell cell : cells) {
             if(cell.getX()==x && cell.getY()==y) {
                 return cell;
             }
+        }*/
+        try {
+            return cells[x][y];
         }
-        return null;
+        catch(ArrayIndexOutOfBoundsException e){
+            return null;
+        }
+        //return null;
     }
     public static Map getinstance() {
         if(map==null) {
@@ -65,7 +80,7 @@ public class Map {
     }
     
     private void setSafeZones() {
-        for(Cell cell : cells) {
+        /*for(Cell cell : cells) {
             //Bottom left
             if(cell.getX()<=5 && cell.getY()<=5) {
                 cell.zone=Zone.SafeZoneMan;
@@ -84,6 +99,30 @@ public class Map {
             }
             else { 
                 cell.zone=Zone.Other; 
+            }
+        }*/
+        //Bottom left
+        for(int i=0; i<SIZE_SAFE_ZONE;i++) {
+            for(int j=0;j<SIZE_SAFE_ZONE;j++) {
+                this.getCell(i, j).zone=Zone.SafeZoneMan;
+            }
+        }
+        //Top right
+        for(int i=LENGTH-SIZE_SAFE_ZONE; i<LENGTH;i++) {
+            for(int j=HEIGHT-SIZE_SAFE_ZONE;j<SIZE_SAFE_ZONE;j++) {
+                this.getCell(i, j).zone=Zone.SafeZoneElf ;
+            }
+        }
+        //Top left
+        for(int i=0; i<SIZE_SAFE_ZONE;i++) {
+            for(int j=HEIGHT-SIZE_SAFE_ZONE;j<HEIGHT;j++) {
+                this.getCell(i, j).zone=Zone.SafeZoneTroll ;
+            }
+        }
+        //Bottom right
+        for(int i=LENGTH-SIZE_SAFE_ZONE; i<LENGTH;i++) {
+            for(int j=0;j<SIZE_SAFE_ZONE;j++) {
+                this.getCell(i, j).zone=Zone.SafeZoneOrc ;
             }
         }
     }
